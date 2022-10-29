bf5924c501af815d9c25d5da71867793d33c9710
ishikawa <t.ishikawa@plan-zaro.co.jp>
2022-10-29 23:57:57 +0900

    first

---
+++ resources/views/tests/component-test1.blade.php
@@ -0,0 +1,4 @@
+{{-- <x-tests.app> --}}
+{{-- コンポーネントテスト1 --}}
+{{-- <x-tests.card title="タイトル" content="本文" :message=$message /> --}}
+{{-- </x-tests.app> --}}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComponentTestController;
+use App\Http\Controllers\LifeCycleTestController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('user.welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('user.dashboard');
+})->middleware(['auth:users'])->name('dashboard');
+
+// Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
+// Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
+Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServiceContainerTest']);
+
+require __DIR__.'/auth.php';
