de333f7e3c3416ce5658126c6143b4fdf78014d5
ishikawa <t.ishikawa@plan-zaro.co.jp>
2022-11-25 01:14:13 +0900

    image index

--- resources/views/components/shop-thumbnail.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    @if (empty($filename))
-        <img src="{{ asset('images/no_image.jpg') }}">
-    @else
-        <img src="{{ asset('strage/shops/' . $filename) }}">
-    @endif
-</div>
