cdbdba08c7a20068559fd63aee9afe80ca8d708c
ishikawa <t.ishikawa@plan-zaro.co.jp>
2022-12-14 14:54:00 +0900

    product update

--- resources/views/owner/products/edit.blade.php
+++ resources/views/owner/products/edit.blade.php
@@ -10,8 +10,10 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                    <x-flash-message status="session('status')" />
                     <form method="post" action="{{ route('owner.products.update', ['product' => $product->id] )}}">
                         @csrf
+                        @method('put')
                         <div class="-m-2">
                             <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
@@ -41,7 +43,7 @@
                                 <div class="relative">
                                     <label for="current_quantity" class="leading-7 text-sm text-gray-600">現在の在庫</label>
                                     <input type="hidden" id="current_quantity" name="current_quantity" value="{{ $quantity }}" >
-                                    <div class="w-full bg-gray-100 bg-opacity-50 rounded focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ $quantity }}</div>
+                                    <div class="w-full bg-gray-100 bg-opacity-50 rounded  text-base outline-none text-gray-700 py-1 px-3 leading-8">{{ $quantity }}</div>
                                 </div>
                             </div>
                             <div class="p-2 w-1/2 mx-auto">
@@ -91,8 +93,8 @@
                             <x-select-image :images="$images" name="image5" />
                             <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative flex justify-around">
-                                    <div><input type="radio" name="is_selling" value="1" class="mr-2" @if($shop->is_selling === 1){ checked } @endif>販売中</div>
-                                    <div><input type="radio" name="is_selling" value="0" class="mr-2" @if($shop->is_selling === 0){ checked } @endif>停止中</div>
+                                    <div><input type="radio" name="is_selling" value="1" class="mr-2" @if($product->is_selling === 1){ checked } @endif>販売中</div>
+                                    <div><input type="radio" name="is_selling" value="0" class="mr-2" @if($product->is_selling === 0){ checked } @endif>停止中</div>
                                 </div>
                             </div>
                             <div class="p-2 w-full flex justify-around mt-4">
