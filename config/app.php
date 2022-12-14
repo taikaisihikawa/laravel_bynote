a76e2d53bb7c41d8059fda799b6f51fb4b5d88b1
ishikawa <t.ishikawa@plan-zaro.co.jp>
2022-12-14 15:23:41 +0900

    constant

--- config/app.php
+++ config/app.php
@@ -230,8 +230,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'InterventionImage' =>
-        Intervention\Image\Facades\Image::class
-
+        Intervention\Image\Facades\Image::class,
+        'Constant' => App\Constants\Common::class
     ],
 
 ];
